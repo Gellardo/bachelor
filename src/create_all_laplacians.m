965b83fd8335fb3a77201dde8af5099c7c0baf97
Frank Schmidt <rubiccuber@googlemail.com>
2014-08-23 15:26:23 +0200

    src: added script to precompute laplacians of the shapes

---
+++ src/create_all_laplacians.m
@@ -0,0 +1,52 @@
+%% done: cat centaur david dog michael wolf
+%% problems: gorilla
+addpath('toolbox_graph','toolbox_graph/toolbox');
+datadir = '~/Data/bachelor/shapes/';
+outdir = '~/Data/bachelor/laplacians/';
+
+convert = 0;
+
+%% convert *.vert + *.tri into *.off
+if convert
+vertfiles = dir(strcat(datadir, '*.vert'));
+trifiles = dir(strcat(datadir, '*.tri'));
+
+for file = vertfiles'
+	[filename ~] = strsplit(file.name, '.vert');
+	filename = filename(1);
+	%check for tri
+	if(strncmp(filename, [trifiles.name],length(filename)))
+		vert = load(strcat(datadir, filename{1},'.vert'));
+		tri = load(strcat(datadir, filename{1},'.tri'));
+		fprintf('loaded %s files to write them\n',filename{1});
+
+		write_off(strcat(datadir, filename{1},'.off'),vert,tri);
+	end
+end
+
+clear filename
+clear file
+clear vertfiles
+clear trifiles
+clear vert
+clear tri
+end
+%% calculate the Laplacian for all of the shapes
+
+files = dir(strcat(datadir,'tosca_v*.off'));
+
+for file = files'
+	[filename, ~] = strsplit(file.name, '.off');
+	filename = filename(1);
+	[M.vert,M.face] = read_off(strcat(datadir, file.name));
+	[eigenfunctions, eigenvalues] = mesh_get_laplacian_eigenfunctions(M.vert,M.face, 200);
+	matf = matfile(strcat(outdir, filename{1},'.mat'), 'Writable', true);
+	matf.name = filename{1};
+	matf.eigenfunctions = eigenfunctions;
+	matf.eigenvalues = eigenvalues;
+    
+    fprintf('finished laplacian for %s\n',file.name);
+
+end
+
+clear all
