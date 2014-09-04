584116c1ee3a6b688ae4488bb3cec1b7b113876e
Frank Schmidt <rubiccuber@googlemail.com>
2014-09-04 18:22:17 +0200

    src: included precomputed laplacian into test_error+ small bug

--- src/test_error.m
+++ src/test_error.m
@@ -2,6 +2,8 @@
 addpath('toolbox_graph','toolbox_graph/toolbox','geodesic_matlab');
 datadir = '~/Data/bachelor/shapes/';
 corrdir = '~/Data/bachelor/corr_shrec2010/';
+laplacedir = '~/Data/bachelor/laplacians/';
+
 meshes = {
 %% careful: porig is the reference on the first given shape
 %% which has to be a the reference shape of the .label files
@@ -20,9 +22,9 @@ porig = 910;
 fid = fopen('~/Data/bachelor/results/error','a+','n','UTF-8');
 fprintf(fid,'\n---------------------%s----------------------------\n',date);
 fprintf(fid,'1 geodesic, 2 diffusion t=0.1, 3 diffusion t=1, 4 commute-time, 5 biharmonic\n');
-fprintf(fid,'\tmax error, avg error\n\n');
+fprintf(fid,'\tmax error, avg error for distances from point %d\n\n', porig);
 %generate distances for all meshes
-for i = 1:size(meshes,1)
+for i = 1:size(meshes,2)
 	%for mesh = meshes
 	[M.vert, M.face] = read_off_mod(strcat(datadir,meshes{i},'.off'));
 	vertcount = size(M.vert,2);
@@ -31,7 +33,7 @@ for i = 1:size(meshes,1)
 		corr = load([corrdir,meshes{i}(11:end),'.labels']);
 		p = find(corr==porig);
 		if(isempty(p))
-			error(['test_error: porig has no correspondence in mesh ',meshes{i}])
+			error(['test_error: porig has no correspondence in mesh ',meshes{i}]);
 		else
 			p = p(1);
 		end
@@ -40,9 +42,17 @@ for i = 1:size(meshes,1)
 		p = porig;
 	end
 
-	%TODO use precomputed laplacians
-
-	[eigenfunctions, eigenvalues] = mesh_get_laplacian_eigenfunctions(M.vert,M.face, 200);
+	%use precomputed laplacians
+    if(exist([laplacedir,meshes{i},'.mat'], 'file'))
+		%load precomputed laplacian
+		matf = matfile([laplacedir,meshes{i},'.mat']);
+		eigenfunctions = matf.eigenfunctions;
+		eigenvalues = matf.eigenvalues;
+		clear matf;
+	else
+		[eigenfunctions, eigenvalues] = mesh_get_laplacian_eigenfunctions(M.vert,M.face, 200);
+    end
+	
 %	[d(1,:), ~] = distance_geodesic(M, p,'exact');
 	opts.type = 'diffusion';
 	opts.t = 0.1;
@@ -59,15 +69,16 @@ for i = 1:size(meshes,1)
 
 	%print it to file
 	fprintf('distances done for %s\n', meshes{i});
+    clear d;
 end
 
 for i = 2:size(m,2)
 	fprintf(fid,'%s:\n',m{i}{1});
 	for j = 1:size(m{1}{2},1)
 		corr = m{i}{3}(:);
-		error = m{1}{2}(1, corr) - m{2}{2}(1,:);
+		error = m{1}{2}(j, corr) - m{i}{2}(j,:);
 		error = error./max(m{1}{2}(j,:));
-		maxerror = max(m{1}{2}(1, corr) - m{2}{2}(1,:));
+		maxerror = max(error);
 		meanerror = mean(error);
 
 		%write stuff
